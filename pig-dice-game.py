d04e944a0cc4b91d3687c7878dd96381f9536a9c
MinjeMun <mmj010220@gmail.com>
2023-01-13 16:36:28 +0900

    feat: Update Edited Code

--- pig-dice-game.py
+++ pig-dice-game.py
@@ -1,11 +1,12 @@
 from random import randint, choice
 from time import sleep
 
+# 게임 시작 함수
 def game_start():
   player_num = int(input('플레이 인원 수를 정해주세요 (1~3) >> '))
   print('컴퓨터를 포함해 총 {}명이 플레이합니다.'.format(player_num + 1))
   print(f'모든 게임의 시작은 컴퓨터가 합니다.')
-  print("=" * 20)
+  print("=" * 30)
 
   global turn
   global order
@@ -30,19 +31,14 @@ def game_start():
 def roll_dice(turn, order, order_limit, bank, score):
     if order == order_limit:
       order = 0
-    
-    print("==== 현재 총 스코어 ====")
-    for i in range(order_limit):
-      print(f'{turn[i]} : {score[i]}점')
-    print("=" * 30)
+  
 
-    print(f'{turn[order]} 차례입니다. [임시점수]: {bank[order]}, [총합점수]: {score[order]}')
+    print(f'>> {turn[order]} 차례입니다. [임시점수]: {bank[order]}, [총합점수]: {score[order]}')
     print('주사위를 굴립니다.')
     sleep(1)
 
     dice = randint(1,6)
-    print(f'{dice}가 나왔습니다.')
-    print("-" * 30)
+    print(f'[{dice}] 나왔습니다.')
 
     bank[order] += dice
 
@@ -50,22 +46,21 @@ def roll_dice(turn, order, order_limit, bank, score):
     if dice == 1:
       print("======= 차례가 넘어갑니다. =======")
       bank[order] = 0
-      change_turn(turn, order)
+      change_turn(turn, order, order_limit)
     else:
-      if score[order] >= 50:
-        sleep(1)
-        print('*' * 30)
-        print(f'{turn[order]}가 승리하셨습니다. 축하합니다.')
-        print('*' * 30)
+      if order == 0:
+        com_choose_roll(turn, order, bank, score)
       else:
-        if order == 0:
-          com_choose_roll(turn, order, bank, score)
-        else:
-          user_choose_roll(turn, order, bank, score)
+        user_choose_roll(turn, order, bank, score)
 
 # 턴 체인지 함수
-def change_turn(turn, order):
+def change_turn(turn, order, order_limit):
   order += 1
+  print(" ")
+  print("========= 현재 총 스코어 =========")
+  for i in range(order_limit):
+    print(f'{turn[i]} : {score[i]}점')
+    print("=" * 34)
   roll_dice(turn, order, order_limit, bank, score)
   # 멈추고 점수 추가 할지 아님 더 굴릴지
 
@@ -76,14 +71,23 @@ def com_choose_roll(turn, order, bank, score):
   com_choose = choice(choose)
 
   if com_choose == 'roll':
-    print(f"{com_choose}로 이어하겠습니다.\n")
+    print(f"{com_choose}로 이어하겠습니다.")
     roll_dice(turn, order, order_limit, bank, score)
     
   else:
-    print(f"{com_choose}로 턴을 넘기겠습니다.\n")
     score[order] += bank[order]
-    print("=" * 30)
-    change_turn(turn, order)
+    bank[order] = 0
+    print("=" * 34)
+
+    if score[order] >= 50:
+      sleep(1)
+      print('*' * 34)
+      print(f'{turn[order]}가 승리하셨습니다. 축하합니다.')
+      print('*' * 34)
+      return print('게임이 종료됩니다.')
+
+    print(f"{com_choose}로 턴을 넘기겠습니다.")
+    change_turn(turn, order, order_limit)
     
 # 유저 선택 함수
 def user_choose_roll(turn, order, bank, score):
@@ -92,14 +96,23 @@ def user_choose_roll(turn, order, bank, score):
     if user_choose == 'roll':
         roll_dice(turn, order, order_limit, bank, score)
     elif user_choose == 'stop':
-      print(f"{user_choose}로 턴을 넘기겠습니다.\n")
       score[order] += bank[order]
-      print("=" * 30)
-      change_turn(turn, order)
+      bank[order] = 0
+      print("=" * 34)
+
+      if score[order] >= 50:
+        sleep(1)
+        print('*' * 34)
+        print(f'{turn[order]}가 승리하셨습니다. 축하합니다.')
+        print('*' * 34)
+        return print('게임이 종료됩니다.')
+
+      print(f"{user_choose}로 턴을 넘기겠습니다.")
+      change_turn(turn, order, order_limit)
     else:
-      print('roll or stop 중 입력하십시오.\n')
-      user_choose_roll(turn, order)
+      print('roll or stop 중 입력하십시오.')
+      user_choose_roll(turn, order, bank, score)
 
-
-
-
+# Main
+if __name__ == "__main__":
+    game_start()
